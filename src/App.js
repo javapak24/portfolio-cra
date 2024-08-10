911e2951ecb4da713e522c616c1b56b1185d9601
javapak24 <ajava2424@gmail.com>
2024-08-10 00:54:33 -0400

    replaced browserrouter with hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import PortfolioView from './PortfolioView';
 import Home from './Home';
 import ContactView from './ContactView';
@@ -7,17 +7,14 @@ import ContactView from './ContactView';
 function App() {
   return (
     <div className="App">
-    <BrowserRouter>
+    <HashRouter>
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/contact' element={<ContactView />} />
       <Route path='/portfolio' element={<PortfolioView />} />
-      <Route path='https://javapak24.github.io/portfolio-cra/contact' element={<ContactView />} />
-      <Route path='https://javapak24.github.io/portfolio-cra/portfolio' element={<PortfolioView />} />
-      <Route path='https://javapak24.github.io/portfolio-cra/home' element={<Home/>} />
       <Route path='/portfolio-cra' element={<Home />} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </div>
   );
 }
--- src/components/MyHeader.js
+++ src/components/MyHeader.js
@@ -10,13 +10,13 @@ export default function MyHeader() {
     <div>
       <Menu fixed='top' inverted>
       <Container>
-        <Menu.Item as='a' href ='/' header>
+        <Menu.Item as='a' href ='#/' header>
         <Image size='mini' src='/logo.png' style={{ marginRight: '1.5em' }} />
         Java
         </Menu.Item>
-        <Menu.Item as='a' href="/">Home</Menu.Item>
-        <Menu.Item as='a' href="/portfolio">Portfolio</Menu.Item>
-        <Menu.Item as='a' href="/contact">Contact</Menu.Item>
+        <Menu.Item as='a' href="#/">Home</Menu.Item>
+        <Menu.Item as='a' href="#/portfolio">Portfolio</Menu.Item>
+        <Menu.Item as='a' href="#/contact">Contact</Menu.Item>
       </Container>
     </Menu>
     </div>
