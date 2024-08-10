74e8e726a7892dd533b4466885db734b2912f2bc
javapak24 <ajava2424@gmail.com>
2024-08-10 00:04:39 -0400

    target = _blank

--- src/components/MyFooter.js
+++ src/components/MyFooter.js
@@ -3,13 +3,13 @@ import { Icon } from 'semantic-ui-react'
 
 const MyFooter = () => (
   <div>
-    <a href='https://facebook.com/' >
-    <Icon bordered color='teal' link name='facebook'/>
+    <a href='https://facebook.com/' target = '_blank'>
+    <Icon bordered color='teal' link name='facebook' />
     </a>
-    <a href='https://linkedin.com/' >
+    <a href='https://linkedin.com/' target = '_blank'>
     <Icon bordered color='teal' link name='linkedin'/>
     </a>
-    <a href='https://x.com/?lang=en' >
+    <a href='https://x.com/?lang=en' target = '_blank'>
     <Icon bordered color='teal' link name='twitter'/>
     </a>
   </div>
