f67f73ad91b796bf49c2445031e4745e4779329e
javapak24 <ajava2424@gmail.com>
2024-08-10 00:41:53 -0400

    .endpoint testing

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/contact' element={<ContactView />} />
       <Route path='/portfolio' element={<PortfolioView />} />
-      <Route path='/portfolio-cra' element={<Home/>} />
+      <Route path='https://javapak24.github.io/portfolio-cra/contact' element={<ContactView />} />
+      <Route path='https://javapak24.github.io/portfolio-cra/portfolio' element={<PortfolioView />} />
+      <Route path='https://javapak24.github.io/portfolio-cra/home' element={<Home/>} />
+      <Route path='/portfolio-cra' element={<Home />} />
     </Routes>
     </BrowserRouter>
     </div>
