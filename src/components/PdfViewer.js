1be857b436dd1cf57952f5780cb7b96eba5970e2
javapak24 <ajava2424@gmail.com>
2025-01-12 00:03:21 -0500

    responsive iframes

--- src/components/PdfViewer.js
+++ src/components/PdfViewer.js
@@ -3,16 +3,9 @@ import React from 'react';
 const PdfViewer = ({ pdfUrl }) => {
   pdfUrl = './case-study-aws.pdf';
   return (
-    <iframe
-      src={pdfUrl}
-      width="100%"
-      height="750px"
-      style={{ border: 'none' }}
-      title="Case Study AWS"
-      scrolling="yes"
-      display="block"
-      overflow="auto"
-    />
+    <div className="responsive-iframe">
+      <iframe src={pdfUrl} title="PDF Viewer" />
+    </div>
   );
 };
 
