566e6d30d497dc3e908a7c743f7f4ee6f2567881
javapak24 <ajava2424@gmail.com>
2025-01-13 20:33:53 -0500

    .

--- src/ContactView.js
+++ src/ContactView.js
@@ -10,6 +10,12 @@ export default function ContactView() {
         <MyHeader></MyHeader>
         <Header as="h1">Contact Me</Header>
         <Segment>
+          I’d love to hear from you! Whether you’re interested in collaborating
+          on a web development project, have an exciting opportunity in tech, or
+          just want to connect and share ideas, feel free to reach out. Let’s
+          create something amazing together!
+        </Segment>
+        <Segment>
           Phone: 774-994-7146
           <br></br>
           <a href="mailto:ajava2424@gmail.com" target="_blank">
